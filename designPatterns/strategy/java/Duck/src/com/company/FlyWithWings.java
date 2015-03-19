26fe56e4f5e34284b2987f224934e09e4223b643
Fil Evgeniy <zheeka.fil@gmail.com>
2015-03-20 00:55:16 +0200

    add strategy pattern

---
+++ designPatterns/strategy/java/Duck/src/com/company/FlyWithWings.java
@@ -0,0 +1,11 @@
+package com.company;
+
+/**
+ * Created by cushty on 1/5/15.
+ */
+public class FlyWithWings implements FlyBehavior {
+    @Override
+    public void fly() {
+        System.out.println("I'm flying!!");
+    }
+}
